a3f17fded6bbe857975417184c4ef0e630e57bdc
Stella-Moradeke <stellaosifila98@gmail.com>
2023-07-26 06:05:37 +0100

    Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,9 +2,7 @@
 word = "Holberton"
 slice0 = word[:3]
 print("First 3 letters: {}".format (slice0))
-
 slice1 = word[-2:]
 print("Last 2 letters: {}".format (slice1))
-
-slice2 = word[-1:1]
+slice2 = word[1:-1]
 print("Middle word: {}".format (slice2))

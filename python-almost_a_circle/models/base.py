569b55438c6567f0edcff20760cd5a479f101ebd
Stella-Moradeke <stellaosifila98@gmail.com>
2023-08-24 18:28:22 +0100

    Bsae Class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+models/base.py:
+This module defines the Base class, which serves as the base class for all other classes in the project.
+"""
 
 class Base:
+    """
+    Base class for managing the id attribute in all future classes.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Constructor for the Base class."""
+
         if id is not None:
             self.id = id
         else:
